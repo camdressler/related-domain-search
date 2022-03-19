fc606c6c59c4d0d4ca278a3dcf6ee0f86c08bc6f
Cam <43306880+camdressler@users.noreply.github.com>
2022-03-18 19:37:48 -0600

    Update controller.js

--- controller.js
+++ controller.js
@@ -2,9 +2,9 @@ const sizeReq = require('./requests/getSize.js')
 const urlReq = require('./requests/getURLs.js')
 
 exports.size = async (domain) => {
-    return await urlReq.run(domain)
+  return await sizeReq.run(domain, page)
 }
 
 exports.url = async (domain, page) => {
-    return await sizeReq.run(domain, page)
+   return await urlReq.run(domain)
 }
